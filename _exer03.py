4706b716e03780659d190af2f261d33b2da7ec2c
Henrique Andrade <henrique.portugal15@gmail.com>
2024-12-26 20:50:08 +0000

    Add files via upload

---
+++ _exer03.py
@@ -0,0 +1,11 @@
+primeiro_valor = input("Digite um valor ")
+segundo_valor = input("Digite outro valor ")
+
+if primeiro_valor < segundo_valor:
+    print("O segundo número é maior")
+elif primeiro_valor > segundo_valor:
+    print("O segundo número é menor")
+elif primeiro_valor >= segundo_valor:
+    print("Os dois número são iguais")
+else:
+    print("")
---
+++ _exer05.py
@@ -0,0 +1,7 @@
+nome = "Henrique"
+idade = 39
+cidade = "Rio de Jnaeiro"
+
+linha_1 = f'Seu nome é {nome}, você tem {idade} anos de idade e mora na cidade do {cidade}'
+
+print(linha_1)
